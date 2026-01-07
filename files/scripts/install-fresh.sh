c39b8c2a1f8b0560c3b43da52b74069d40266b9a
Martin Schumann <mfs@mfs.name>
2026-01-07 20:23:59 +0100

    install fresh & gitbutler as rpm in silverblue

--- files/scripts/install-fresh.sh
+++ files/scripts/install-fresh.sh
@@ -3,6 +3,6 @@ curl -sL $(\
     curl -s https://api.github.com/repos/sinelaw/fresh/releases/latest \
     | grep "browser_download_url.*\.$(uname -m)\.rpm" \
     | cut -d '"' -f 4\
-    ) -o fresh-editor.rpm \
-&& dnf install -y ./fresh-editor.rpm \
-&& rm ./fresh-editor.rpm
+    ) -o tmp.rpm \
+&& dnf install -y ./tmp.rpm \
+&& rm ./tmp.rpm
